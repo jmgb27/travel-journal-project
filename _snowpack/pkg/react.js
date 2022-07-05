36b01c8ee64bbde16a81325882cb30ee17396279
jmgb27 <mark.john6672@gmail.com>
2022-07-05 21:03:40 +0800

    Updates

---
+++ _snowpack/pkg/react.js
@@ -0,0 +1,8 @@
+import { r as react } from './common/index-ef047052.js';
+export { r as default } from './common/index-ef047052.js';
+
+
+
+var useEffect = react.useEffect;
+var useState = react.useState;
+export { useEffect, useState };
